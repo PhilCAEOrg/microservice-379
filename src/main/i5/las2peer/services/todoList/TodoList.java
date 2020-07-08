f25b820c89ed045abba63967512fc22b598a140a
PhilCAEUser <mail@rwth>
2020-07-08 08:11:58 +0000

    Http Response[response] edited by Philipp Dolif

--- src/main/i5/las2peer/services/todoList/TodoList.java
+++ src/main/i5/las2peer/services/todoList/TodoList.java
@@ -155,23 +155,7 @@ public class TodoList extends RESTService {
 
 
      
-    // service method invocations
-
-     
-
-
-
-
-    // response
-    boolean response_condition = true;
-    if(response_condition) {
-      JSONObject result = new JSONObject();
-
-      
-
-      return Response.status(HttpURLConnection.HTTP_OK).entity(result.toJSONString()).build();
-    }
-    return null;
+    
   }
 
 
